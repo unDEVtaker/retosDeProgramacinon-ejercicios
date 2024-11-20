5a0b1111c62e88ff5df74b1fd5a183e852f7ff4e
Jona Silva <jonatanemanuelsilva6@gmail.com>
2024-11-20 09:01:51 -0300

    add correcion 1FizzBuzz.js

--- 1FizzBuzz.js
+++ 1FizzBuzz.js
@@ -8,18 +8,13 @@
  */
 
 function fizzBuzz() {
-    for (let i = 1; i <= 100; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
-            console.log("fizzbuzz");
-        } else if(i % 3 === 0) {
-            console.log("fizz");
-        } else if(i % 5 === 0) {
-            console.log("buzz");
-        }
-        else {
-            console.log(i);
-        }   
-    }
+  for (let i = 1; i <= 100; i++) {
+    let output = "";
+    if (i % 3 === 0 && i % 5 === 0) output += "fizzbuzz";
+    if (i % 3 === 0) output += "fizz";
+    if (i % 5 === 0) output += "buzz";
+    console.log(output || i);
+  }
 }
 
 fizzBuzz();
